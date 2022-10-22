6e5f8e5e36d5d376ba4b28534daf23d13fc20ac9
husna <hpm25@georgetown.edu>
2022-10-21 17:15:30 -0700

    add display messages

---
+++ display.rb
@@ -0,0 +1,38 @@
+module Display
+
+  def title 
+    puts "Welcome to Hangman. Guess the hidden word by entering a letter for each turn."
+    puts "After selecting 10 incorrect letters, you will hang (lose)!"
+  end
+  
+  def intro
+    puts "Would you like to: \n1) start a new game \n2) load a previous game."
+  end
+
+  def user_selection
+    answer = gets.chomp
+    return answer if answer.match?(/^[1-2]$/)
+    intro
+  end
+
+  def instruction
+    "Enter a letter or Enter 'save' to save your game for next time."
+  end
+
+  def saved_message
+    "Your game has been saved. Remember what you named your game."
+  end 
+
+  def invalid_guess
+    "Invalid. Please enter a single letter only. "
+  end 
+
+  def wrong_guess_message
+    puts "Better luck next time!"
+  end 
+
+  def correct_letter_message
+    puts "That's it!"
+  end 
+
+end
