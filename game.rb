1156e0471b27d01b6a1373216eb33386d91b3dfd
husna <hpm25@georgetown.edu>
2022-10-21 20:06:50 -0700

    get game mode and random word

--- game.rb
+++ game.rb
@@ -1,18 +1,43 @@
-class Game 
+require_relative 'display.rb'
+
+class Game
   include Display
-  
+
   def initialize
     @available_letters = ('a'..'z').to_a
     @letters_guessed = []
     @guesses_remaining = 10
+    @mode = nil
     play_game
   end
 
-  def play_game 
+  def play_game
     title
     intro
-    new_game if user_selection == '1'  
-    load_game if user_selection == '2'
+    mode = user_selection
+    new_game if mode == '1'
+    load_game if mode == '2'
   end
 
-end 
+  def user_selection
+    answer = gets.chomp
+    return answer if answer.match?(/^[1-2]$/)
+    puts "Invalid Selection. Please enter '1' or '2'."
+    user_selection
+  end
+  
+  def new_game
+    random_word
+    
+  end
+
+  def load_game; end
+
+  def random_word
+    words = []
+    File.open("word_list.txt").readlines.each { |line| words.push(line) if line.length.between?(6, 12) }
+    @word = words.sample
+    puts @word
+    create_blank_spaces(@word)
+  end
+end
