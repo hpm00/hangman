91eb5d69c4058dd677398eab0d4623cd8b0a8dff
husna <hpm25@georgetown.edu>
2022-10-21 20:56:30 -0700

    add outro messages

--- display.rb
+++ display.rb
@@ -1,38 +1,40 @@
 module Display
 
-  def title 
-    puts "Welcome to Hangman. Guess the hidden word by entering a letter for each turn."
-    puts "After selecting 10 incorrect letters, you will hang (lose)!"
-  end
+    def title 
+      puts "Welcome to Hangman. Guess the hidden word by entering a letter for each turn."
+      puts "After selecting 10 incorrect letters, you will hang (lose)!"
+    end
+    
+    def intro
+      puts "\nWould you like to: \n1) start a new game \n2) load a previous game."
+    end
   
-  def intro
-    puts "Would you like to: \n1) start a new game \n2) load a previous game."
-  end
-
-  def user_selection
-    answer = gets.chomp
-    return answer if answer.match?(/^[1-2]$/)
-    intro
-  end
-
-  def instruction
-    "Enter a letter or Enter 'save' to save your game for next time."
-  end
-
-  def saved_message
-    "Your game has been saved. Remember what you named your game."
-  end 
-
-  def invalid_guess
-    "Invalid. Please enter a single, non-selected letter only."
-  end 
-
-  def wrong_guess_message
-    puts "Better luck next time!"
-  end 
-
-  def correct_letter_message
-    puts "That's it!"
-  end 
-
+    def instruction
+      "\nEnter a letter or Enter 'save' to save your game for next time."
+    end
+  
+    def saved_message
+      "Your game has been saved. Remember what you named your game."
+    end 
+  
+    def invalid_guess
+      "Invalid. Please enter a single letter that has not been guessed."
+    end 
+  
+    def wrong_guess_message
+      puts "\nUnlucky! The word does not have that letter."
+    end 
+  
+    def correct_letter_message
+      puts "\nNice guess."
+    end 
+  
+    def winner
+      puts "\nYou guessed the word! You've been spared!"
+    end 
+  
+    def loser
+      puts "\nYou didn't guess the word. Better luck next time."
+    end
+    
 end
